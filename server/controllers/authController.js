1a14ecb9f101b2a23815549192b6ca6d930f27c1
Rahat Ahmed Khan <rahatofficial.29@gmail.com>
2026-05-06 15:49:29 +0600

    setup refresh token api

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,11 @@
+// @ts-nocheck
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 import {findUserbyEmail} from '../models/userModel.js'
 import {createUser} from '../models/userModel.js'
 import { generateAccessToken, generateRefreshToken } from '../utils/jwt.js';
-
+dotenv.config()
 // @ts-ignore
 export  const  register=async (req,res)=>{
     const {name,email,password}=req.body;
@@ -10,19 +13,19 @@ export  const  register=async (req,res)=>{
        const userInfo=await findUserbyEmail(email)
        // @ts-ignore
        if(userInfo.length>0){
-        res.status(400).json({message:"User already exists"})
+        return res.status(400).json({message:"User already exists"})
 
        }
        const salt=10
        const hashedPassword=await bcrypt.hash(password,salt)
        const role='user'
        const result=await createUser(name,email,hashedPassword,role);
-       if(result)res.status(201).json({message:"User created succesfully",
+       if(result){return res.status(201).json({message:"User created succesfully",
         // @ts-ignore
         userId:result.insertId
-       })
+       })}
     }catch(err){
-        res.json({message:"error from authController.register",
+        return res.json({message:"error from authController.register",
             error:err
 
         })
@@ -35,15 +38,15 @@ export  const  register=async (req,res)=>{
 export const login=async(req,res)=>{
     const {email,password}=req.body
     try{
-        if(!email||!password){res.status(400).json({message:"enter credentials"})}
+        if(!email||!password){return res.status(400).json({message:"enter credentials"})}
      const queryResult=await findUserbyEmail(email)
      // @ts-ignore
-     if(!queryResult.length){res.status(400).json({message:"Invalid email or password"})}
+     if(!queryResult.length){return res.status(401).json({message:"Invalid email or password"})}
     // @ts-ignore
     const user=queryResult[0];
    
     const isMatch=await bcrypt.compare(password,user.password)
-    if(!isMatch)res.status(401).json({message:"Wrong password"})
+    if(!isMatch)return res.status(401).json({message:"Wrong password"})
        const accessToken=generateAccessToken(user)
     const refreshToken=generateRefreshToken(user)
 
@@ -52,15 +55,29 @@ export const login=async(req,res)=>{
     secure:false,
     sameSite:'strict'
         })
-        res.json({
+         res.json({
             message:"Login Successful",
             accessToken
         })
 
     }catch(err){
-        res.status(500).json({message:"error thrown from db at authController.login",
-            error:err
+        return res.status(500).json({message:"error thrown from db at authController.login",
+            
         })
     }
 }
     
+export const refresh=(req,res)=>{
+    const refreshToken=req.cookies.refreshToken
+    if(!refreshToken) return res.status(401).json({ message: "No refresh token" })
+        try{
+    const user=jwt.verify(refreshToken,process.env.JWT_REFRESH_SECRET)
+    const newAccessToken=generateAccessToken({
+        id:user.id,
+        role:user.role
+    })
+    res.json({accessToken:newAccessToken})
+    }catch(err){
+        return res.status(403).json({message: "Invalid refresh token"})
+    }
+}
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import {login, register} from '../controllers/authController.js'
+import {login, refresh, register} from '../controllers/authController.js'
 
 const authRouter=express.Router()
 
 authRouter.post('/register',register)
 authRouter.post('/login',login)
+authRouter.post('/refresh',refresh)
